add37b50ade1fafbcd2e9493e3f4d3513a5c9670
A477MZZ <Treasure2013>
2014-02-26 14:05:39 -0600

    Geolocation/maps

--- app/js/Controllers/User/GoogleMapsDemo.js
+++ app/js/Controllers/User/GoogleMapsDemo.js
@@ -4,23 +4,38 @@ angular.module('LendYourTools.controllers', []).
 
             $scope.coords = { lat: 44, long: -45 };
             $scope.map = {
+                zoom: 8,
                 center: {
                     latitude: $scope.coords.lat,
                     longitude: $scope.coords.long
                 },
-                zoom: 17,
-                draggable: true
+                options: {
+                   //mapTypeId: google.maps.MapTypeId.TERRAIN
+                },
+                events: {
+                    //click: function() { alert("hi")}
+                    },
+                pin: 'http://maps.google.com/mapfiles/marker.png',
             };
+            $scope.map.infoWindow = { show: true, coords: $scope.map.center }
+            $scope.map.marker = {
+                coords: angular.copy($scope.map.center),
+                options: { draggable: true },
+                events: { dragend: function(im,event,Arguments) {
+                    debugger
+                    var mscope = $('#m').scope();
+                    var loc = { latitude: im.position.d, longitude: im.position.e };
+                    alert(loc.latitude + " " + $scope.map.marker.coords.latitude )
+                }
+                }};
 
             GeoLocationService.GetLocation().then(function(data){
+                $scope.coords = {latitude:data.coords.latitude, longitude:data.coords.longitude};
+                $scope.map.center.latitude =$scope.coords.latitude;
+                $scope.map.center.longitude =$scope.coords.longitude;
                 debugger
-                $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
-                $scope.map.center.latitude =$scope.coords.lat;
-                $scope.map.center.longitude =$scope.coords.long;
-                console.log("at ")
-                console.log($scope.map.center)
+                $scope.map.marker.coords = angular.copy($scope.coords);
             });
 
-            $scope.pin='http://maps.google.com/mapfiles/marker.png';
 
     }])
